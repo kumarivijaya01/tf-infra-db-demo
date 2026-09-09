afbb4b2aed7c0ad2093ecfacf2ce203a43d16261
kumarivijaya01 <kumari.vijaya@telus.com>
2026-09-10 00:25:28 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_version = ">= 1.3"
-  backend "gcs" {
-    bucket = "tf-state-cio-esr-np-12345"
-    prefix = "terraform/np"
-  }
-}
--- terraform/pgi-esr-st/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_version = ">= 1.3"
-  backend "gcs" {
-    bucket = "tf-state-cio-esr-np-123456"
-    prefix = "terraform/np"
-  }
-}
