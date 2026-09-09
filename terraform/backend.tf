fd3688360d09429d134ea96c42a352a8efc92bdb
kumarivijaya01 <kumari.vijaya@telus.com>
2026-09-10 00:37:55 +0530

    xlator: provision pgi-esr-st
    Files: terraform.tfvars, variables.tf, cloudsql.tf, iam.tf, provider.tf, backend.tf
    Generated by Xlator at 2026-09-09T19:07:54.421Z

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.3"
   backend "gcs" {
-    bucket = "tf-state-cio-esr-np-123456"
+    bucket = "tf-state-cio-esr-np1234455"
     prefix = "terraform/np"
   }
 }
