e326a4908552f850061d6a85c2008bd6bc7b84a6
kumarivijaya01 <kumari.vijaya@telus.com>
2026-09-17 16:24:16 +0530

    xlator: pgi-esr-st

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,7 @@
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 3.61.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = ">= 3.61.0"
-    }
+    random = { source = "hashicorp/random"; version = ">= 3.0.0" }
+    google = { source = "hashicorp/google"; version = ">= 3.61.0" }
+    google-beta = { source = "hashicorp/google-beta"; version = ">= 3.61.0" }
   }
 }
