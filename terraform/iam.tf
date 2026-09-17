5db7f5003ed8060f5c1df310c385a116ed18b7f7
kumarivijaya01 <kumari.vijaya@telus.com>
2026-09-17 16:26:52 +0530

    xlator: update pgi-esr-st

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -34,3 +34,13 @@ resource "google_project_iam_member" "cloudsql_roles" {
   role    = each.value
   member  = "group:dl_clouddba@telus.com"
 }
+
+# IAM update for pgi-esr-st
+
+resource "google_sql_user" "pgi-esr-st-iam-users" {
+  for_each = var.env == "np" ? var.cloud_sql_iam_users : {}
+  project  = var.project_id
+  name     = each.key
+  instance = module.pgi-esr-st.instance_name
+  type     = each.value
+}
